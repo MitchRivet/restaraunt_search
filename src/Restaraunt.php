df702e86c5c6c3798678046b459fc548ecf4b5fb
Kevin Tokheim & Mitch Rivet <student@epicodus.com>
2015-08-19 12:07:29 -0700

    add initial files and save, getall, and delete all functions

---
+++ src/Restaraunt.php
@@ -0,0 +1,65 @@
+<?php
+
+class Restaraunt
+{
+
+    private $restaraunt_name;
+    private $id;
+
+    //Constructor
+    function __construct($restaraunt_name, $id = null)
+    {
+        $this->restaraunt_name = $restaraunt_name;
+        $this->id = $id;
+    }
+
+    //Setters
+    function setRestarauntName($new_restaraunt_name)
+    {
+        $this->restaraunt_name = (string) $new_restaraunt_name;
+    }
+
+    //Getters
+    function getRestarauntName()
+    {
+        return $this->restaraunt_name;
+    }
+
+    function getId()
+    {
+      return $this->id;
+    }
+
+    //Save function
+    function save()
+    {
+          $GLOBALS['DB']->exec("INSERT INTO restaraunts (restaraunt_name) VALUES ('{$this->getRestarauntName()}');");
+          $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    //Static getAll function
+    static function getAll()
+    {
+        $returned_restaraunts = $GLOBALS['DB']->query("SELECT * FROM restaraunts;");
+        $restaraunts = array();
+        foreach ($returned_restaraunts as $restaraunt) {
+
+            $restaraunt_name = $restaraunt['restaraunt_name'];
+            $id = $restaraunt['id'];
+            $new_restaraunt = new Restaraunt ($restaraunt_name, $id);
+            array_push($restaraunts, $new_restaraunt);
+        }
+        return $restaraunts;
+    }
+
+    //Static deleteAll function
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM restaraunts;");
+    }
+
+
+}
+
+
+?>
---
+++ tests/RestarauntTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+require_once "src/Restaraunt.php";
+
+$server = 'mysql:host=localhost;dbname=restaraunt_search_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
+class RestarauntTest extends PHPUnit_Framework_TestCase
+{
+
+    protected function tearDown()
+    {
+      Restaraunt::deleteAll();
+    }
+
+    function test_save()
+    {
+        $restaraunt_name = "Yum Yum Kitchen";
+        $test_restaraunt = new Restaraunt($restaraunt_name);
+
+        $test_restaraunt->save();
+
+        $result = Restaraunt::getAll();
+        $this->assertEquals($test_restaraunt, $result[0]);
+    }
+
+    function test_getAll()
+    {
+      $restaraunt_name = "Yum Yum Kitchen";
+      $restaraunt_name2 = "Hot Dog Heaven";
+      $test_restaraunt = new Restaraunt($restaraunt_name);
+      $test_restaraunt->save();
+      $test_restaraunt2 = new Restaraunt($restaraunt_name2);
+      $test_restaraunt2->save();
+
+      $result = Restaraunt::getAll();
+
+      $this->assertEquals([$test_restaraunt, $test_restaraunt2], $result);
+
+    }
+
+    function test_deleteAll()
+    {
+      $restaraunt_name = "Yum Yum Kitchen";
+      $restaraunt_name2 = "Hot Dog Heaven";
+      $test_restaraunt = new Restaraunt($restaraunt_name);
+      $test_restaraunt->save();
+      $test_restaraunt2 = new Restaraunt($restaraunt_name2);
+      $test_restaraunt2->save();
+
+      Restaraunt::deleteAll();
+
+      $result = Restaraunt::getAll();
+      $this->assertEquals([], $result);
+    }
+
+    function test_getId()
+    {
+      $restaraunt_name = "Yum Yum Kitchen";
+      $id = 1;
+      $test_restaraunt = new Restaraunt($restaraunt_name, $id);
+
+      $result = $test_restaraunt->getId();
+
+      $this->assertEquals(1, $result);
+    }
+
+
+
+
+
+
+
+
+
+}
+?>
